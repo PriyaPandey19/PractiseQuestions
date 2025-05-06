54ff721e77a1d772cfdaa7c3eb36a8b3c6d91d30
PriyaPandey19 <pp479021@gmail.com>
2025-05-06 13:50:44 +0530

    bye

--- PractiseSheet.java
+++ PractiseSheet.java
@@ -1,5 +1,7 @@
 public class PractiseSheet{
     public static void main(String args[]){
-        
+       System.out.println("Hello ");
+       System.out.println("Bye");
+       System.out.println("HYY"); 
     }
 }
