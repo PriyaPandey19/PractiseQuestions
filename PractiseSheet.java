5693cbf3bb2db85d664f9a61440c8550c0a5d6e5
PriyaPandey19 <pp479021@gmail.com>
2025-05-07 13:02:47 +0530

    first commit

--- PractiseSheet.java
+++ PractiseSheet.java
@@ -1,7 +1,5 @@
 public class PractiseSheet{
     public static void main(String args[]){
-       System.out.println("Hello ");
-       System.out.println("Bye");
-       System.out.println("HYY"); 
+      System.out.println("hyy");
     }
 }
