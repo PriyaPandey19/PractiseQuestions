f30c5cb5b2dcedfcf2a053981729c5e3960c8cb0
PriyaPandey19 <pp479021@gmail.com>
2025-06-13 22:11:16 +0530

    by

--- PractiseSheet2DArray.java
+++ PractiseSheet2DArray.java
@@ -114,6 +114,41 @@ public static void rotate90Clockwise(int[][] matrix) {
     }
 
 
+    public static int[][] multiplyMatrices(int[][] A, int[][] B) {
+        int rowsA = A.length;
+        int colsA = A[0].length;
+        int colsB = B[0].length;
+
+        // Initialize result matrix
+        int[][] result = new int[rowsA][colsB];
+
+        // Matrix multiplication logic
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return result;
+    }
+    
+     public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+   
+
+
+
 
     
         
@@ -121,12 +156,21 @@ public static void rotate90Clockwise(int[][] matrix) {
     public static void main(String[] args) {
        
  
-        int[][] mat = {
+        int[][]mat = {
             {1, 2, 3},
             {4, 5, 6},
             {7, 8, 9}
         };
-        System.out.println(searchMatrix(mat, 8));
+        int [][] mat2 ={
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int[][] result = multiplyMatrices(mat, mat2);
+        System.out.println("Matrix Multiplication Result:");
+        printMatrix(result);
+        
+       // System.out.println(searchMatrix(mat, 8));
         // System.out.println("original matrix is:");
         // for (int i = 0; i < mat.length; i++) {
         //     System.out.println(Arrays.toString(mat[i]));
