8a1be17940bd618bf08069845d7ed4fe8ad3a64d
PriyaPandey19 <pp479021@gmail.com>
2025-06-16 21:40:20 +0530

    bye

--- PractiseSheet2DArray.java
+++ PractiseSheet2DArray.java
@@ -184,6 +184,39 @@ public static void printPascalTriangle(int n) {
 
 
 
+ public static int numIslands1(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length; 
+      for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+               
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void dfs(char[][] grid, int i, int j) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
+    }
+
+
+
+
 
    
 
@@ -194,8 +227,15 @@ public static void printPascalTriangle(int n) {
         
 
     public static void main(String[] args) {
-        int n = 5; // Number of rows
-    printPascalTriangle(n);
+         char[][] grid = {
+            {'1','1','0','0','0'},
+            {'1','1','0','0','0'},
+            {'0','0','1','0','0'},
+            {'0','0','0','1','1'}
+        };
+
+        System.out.println("Number of islands: " + numIslands(grid)); // Output: 3
+      
        
  
         // int[][]mat = {
