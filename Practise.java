138da5993d72d8462ed5ecf146a4511a1dbb902d
PriyaPandey19 <pp479021@gmail.com>
2025-06-26 21:26:19 +0530

    practise

---
+++ Practise.java
@@ -0,0 +1,26 @@
+ public class Practise{
+ 
+ public static void findSubsets(String str, int i, String ans){     //find all possible subsets
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            }
+            else{
+                System.out.println(ans);
+            }
+            return;
+        }
+
+         //yes choice
+        findSubsets(str, i+1, ans+str.charAt(i));
+        //no choice
+        findSubsets(str, i+1, ans);
+    }
+    public static void main(String[] args) {
+      String str = "abc";
+    findSubsets(str, 0, "");   
+    }
+ 
+ }
+    
+
