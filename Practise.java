4c4011696e028fc28fc98b2808d280d062d1aa19
PriyaPandey19 <pp479021@gmail.com>
2025-06-29 10:52:24 +0530

    practise

--- Practise.java
+++ Practise.java
@@ -115,6 +115,26 @@ public static void countingSort(int arr[]){    //counting sort
   }
 
 
+  public static void modifiedbubbleSort(int arr[]){      // Optimized  bubble sort
+    for(int turn =0;turn<arr.length-1;turn++){
+       boolean swapped = false;
+      for(int j =0;j<arr.length-1-turn;j++){
+
+         if(arr[j] > arr[j+1]) {
+           
+             int temp = arr[j];
+             arr[j] = arr[j+1];
+             arr[j+1] = temp;
+             swapped = true;
+         }
+      }
+      if(swapped == false){
+        break;
+      }
+    }
+  }
+
+
 
     public static void main(String[] args) {
      int[] arr = {4, 2, 2, 8, 3, 3, 1};
