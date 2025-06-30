0fbba77fd9346d2ea4562c4afe24efbd0c05f370
PriyaPandey19 <pp479021@gmail.com>
2025-06-30 21:02:40 +0530

    practise

--- Practise.java
+++ Practise.java
@@ -139,6 +139,39 @@ public static void countingSort(int arr[]){    //counting sort
 
 
 
+
+
+  public static void  quickSort(int arr[],int si,int ei){   //quick sort
+      if(si >= ei){   //base case
+        return;
+      }
+      int pidx = partition(arr, si,ei);
+      quickSort(arr, si, pidx-1);   //left half
+      quickSort(arr, pidx+1, ei);   //right half
+    }
+
+    public static int partition(int arr[],int si,int ei){
+    int pivot = arr[ei];
+    int i= si-1;   //to make place for els smaller than pivot
+
+    for(int j=si;j<ei;j++){
+        if(arr[j] <= pivot){
+            i++;
+            //swap
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    i++;
+    int temp = pivot;
+    arr[ei] = arr[i];
+    arr[i] = temp;
+    return i;
+    }
+
+
+
     public static void main(String[] args) {
       int[] arr = {4, 2, 2, 8, 3, 3, 1};
     System.out.println("Original array:");
